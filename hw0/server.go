7cb1f7dd36757d7cdc8db0deebffbc2851b5b636
Ibrahim <abasbekovi@yandex.ru>
2026-07-23 21:12:11 +0300

    added graceful shutdown

--- hw0/server.go
+++ hw0/server.go
@@ -1,26 +1,59 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
 
+const shutdownTimeout time.Duration = 10 * time.Second
+
 func main() {
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		syscall.SIGINT,
+		syscall.SIGTERM,
+	)
+	defer stop()
+
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatalf("failed to listen on port 8080: %v", err)
 	}
-
 	defer listener.Close()
+
+	wg := sync.WaitGroup{}
+
+	go func() {
+		<-ctx.Done()
+
+		log.Println("shutdown signal received")
+
+		err := listener.Close()
+		if err != nil && !errors.Is(err, net.ErrClosed) {
+			log.Printf("failed to close listener: %v", err)
+		}
+	}()
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Printf("error on accept happened: %v\n", err)
+			if ctx.Err() != nil || errors.Is(err, net.ErrClosed) {
+				break
+			}
+
+			log.Printf("failed to accept connection: %v", err)
 			continue
 		}
 
+		wg.Add(1)
 		go func(c net.Conn) {
+			defer wg.Done()
 			defer c.Close()
 
 			if _, err := c.Write([]byte("OK\n")); err != nil {
@@ -28,5 +61,23 @@ func main() {
 				return
 			}
 		}(conn)
+	}
+
+	done := make(chan struct{})
+
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	timer := time.NewTimer(shutdownTimeout)
+	defer timer.Stop()
+
+	select {
+	case <-done:
+		log.Println("server stopped gracefully")
+	
+	case <-timer.C:
+		log.Printf("shutdown timeout of %s exceeded", shutdownTimeout)
 	}
 }
